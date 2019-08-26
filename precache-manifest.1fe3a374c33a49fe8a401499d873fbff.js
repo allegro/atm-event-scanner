4516a41590112d296a06b93a97f54820eaad2333
Bartosz Galek <bartosz@galek.com.pl>
2019-08-26 13:30:48 +0200

    Updates

---
+++ precache-manifest.1fe3a374c33a49fe8a401499d873fbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6780073ee02e970abda3b070039291f",
+    "url": "/atm-event-app/scanner/index.html"
+  },
+  {
+    "revision": "eaf8d24665b0fe03cc6d",
+    "url": "/atm-event-app/scanner/static/css/main.aafb7e44.chunk.css"
+  },
+  {
+    "revision": "84659629058c03e0e743",
+    "url": "/atm-event-app/scanner/static/js/2.d6905ba8.chunk.js"
+  },
+  {
+    "revision": "eaf8d24665b0fe03cc6d",
+    "url": "/atm-event-app/scanner/static/js/main.1379356b.chunk.js"
+  },
+  {
+    "revision": "3e0af73a4b1e7a16ca14",
+    "url": "/atm-event-app/scanner/static/js/runtime~main.cc592acb.js"
+  },
+  {
+    "revision": "1798b9731a4f87231d41c8ccf92e14a7",
+    "url": "/atm-event-app/scanner/static/media/scanner.1798b973.svg"
+  }
+]);
