a37a062a080c6c1f556eb9fc4034e997dfdeb7f1
Bartosz Gałek <bartosz@galek.com.pl>
2022-09-07 23:27:41 +0200

    base url fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./atm-event-scanner",
+    base: "https://allegro.github.io/atm-event-scanner/",
     plugins: [
         react()
     ]
