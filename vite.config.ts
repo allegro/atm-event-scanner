09646cd36f3aaad6ad18bd906b17839b010b8f06
Bartosz Gałek <bartosz@galek.com.pl>
2022-09-07 23:26:38 +0200

    pwa plugin removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./atm-event-scanner",
     plugins: [
-        react(),
-        VitePWA({
-            includeAssets: ['logo.svg', 'apple-touch-icon.png'],
-            manifest: {
-                name: 'ATM Ticket Scanner',
-                short_name: 'ATM Scanner',
-                description: 'ATM Ticket Scanner',
-                theme_color: '#ffffff',
-                icons: [
-                    {
-                        src: 'logo.svg',
-                        sizes: 'any',
-                        type: 'image/svg+xml'
-                    }
-                ]
-            }
-        })
+        react()
     ]
 })
